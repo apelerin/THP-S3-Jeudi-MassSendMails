3f54121ff7b8f8e99a053b62e46552e30039a823
Juliette Audema <juliette.audema@gmail.com>
2018-10-18 19:16:27 +0200

    Commentaires

--- lib/app/townhalls_adder_to_db.rb
+++ lib/app/townhalls_adder_to_db.rb
@@ -2,8 +2,12 @@ require 'csv'
 require 'townhalls_scrapper.rb'
 require 'rubygems'
 
+#Création de la classe AdderToDb
 
 class AdderToDb
+
+#Méthode qui enregistre les mails dans un csv
+
  def save_the_scrapper_in_csv
   towns = [
     'http://www.annuaire-des-mairies.com/seine-saint-denis.html',
@@ -14,12 +18,14 @@ class AdderToDb
     'http://www.annuaire-des-mairies.com/gard-2.html'
   ]
 
+  #On crée l'objet scrapper qui va créer l'array avec tous les hashes des mairies que l'on souhaite
   scrapper = Scrapper.new(towns)
   begin
   scrapper.array_of_emails = scrapper.compute
   rescue OpenURI::HTTPError => e
   return nil
   end 
+  #Insertion des mairies, mails et départements dans un tableau csv
   CSV.open("db/database.csv", "wb") do |csv|
     scrapper.array_of_emails.each do |hashes|
       csv <<	["#{hashes[:name]}", "#{hashes[:townhall_mail]}", "#{hashes[:zip_code]}"]
--- lib/app/townhalls_scrapper.rb
+++ lib/app/townhalls_scrapper.rb
@@ -2,16 +2,18 @@ require 'nokogiri'
 require 'open-uri'
 require 'rubygems'
 
+#Création de la classe Scrapper
 
 class Scrapper
 	attr_accessor :array_urls, :array_of_emails
 
+#La classe est initialisée avec un array d'urls (dans notre cas, ce sont les urls des départements dont on doit scrapper les mails des villes)  
 	def initialize(array_urls)
     @array_urls = array_urls
     @array_of_emails = []
   end
 
-	#Méthode pour récupérer l'adresse email d'une mairie à partir de sa page
+	#Méthode pour récupérer l'adresse email d'une mairie à partir de sa page (va récupère le mail grâce à un xpath)
   def get_the_email_of_a_townhall_from_its_webpage(page_url) 
     begin
     data = Nokogiri::HTML(open(page_url))
@@ -23,26 +25,27 @@ class Scrapper
 		end
 	end
 
-	#Méthode pour récupérer les liens de pages relatives à chaque mairie et qui exécuter la première méthode sur la base des liens récupérés
+	#Méthode pour récupérer les liens de pages relatives à chaque mairie et qui exécute la première méthode sur la base des liens récupérés
 	def get_all_the_urls_of_region_townhalls(page_url)
     get_townhalls_URL = Nokogiri::HTML(open(page_url))
-		get_townhalls_URL.xpath('//a[@class = "lientxt"]').each do |url| #Solution avec XPATH autrement au-dessus en commentrequire "spreadsheet_manager.rb"
-			link = url['href']
-			link[0] = '' #On enlève les points sur les URLs récupérées 
+		get_townhalls_URL.xpath('//a[@class = "lientxt"]').each do |url| 
+			link = url['href']  #récupère l'url
+			link[0] = '' #On enlève le points sur l'URL récupérée
       link = "http://annuaire-des-mairies.com#{link}" #On formatte au bon format d'URL
-      zipcode = get_townhalls_URL.css('.lientitre').text
-      zip_array = zipcode.split
-      zip_array = zip_array.pop
+      zipcode = get_townhalls_URL.css('.lientitre').text #On va récupérer sur la page le nom de département
+      zip_array = zipcode.split #On transforme le string en array de différents éléments
+      zip_array = zip_array.pop #L'array est sous forme ["Département", "de", "Seine-Saint-Denis"], avec .pop on récupère le dernier élément de l'array, le nom du département, qui nous intéresse
       hash_townhall_info = {:name => url.text, :townhall_mail => get_the_email_of_a_townhall_from_its_webpage(link), :zip_code => zip_array} #Création de hash 
-      @array_of_emails << hash_townhall_info #Ajout du hash dans un tableau
+      @array_of_emails << hash_townhall_info #Ajout du hash dans un tableau. Celui-ci est un attribut de classe. A chaque fois que cette méthode tourne, elle ajoute à la suite les hashes.
       end 
     end 
   
+   #Méthode globale qui permet de récupérer les hashes des mairies de chaque département
   def compute
     @array_urls.each do |url|
-      get_all_the_urls_of_region_townhalls(url)
+      get_all_the_urls_of_region_townhalls(url) #Fait passer toutes les URLs du hash donné dans la méthode d'au-dessus.
     end  
-    return @array_of_emails      
+    return @array_of_emails #La méthode compute retourne l'array contenant tous les hashes des mairies.
   end  
 
 end
